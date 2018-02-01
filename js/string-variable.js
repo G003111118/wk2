06f571ccf33291aab047d5e302b0540c54db4d35
G003111118 <35809339+G003111118@users.noreply.github.com>
2018-02-01 15:41:48 +0000

    version1

---
+++ js/string-variable.js
@@ -0,0 +1,16 @@
+
+
+var username;
+var message;
+
+username=' Stephen'
+message= "Hey " + username +' See our upcoming range';
+
+var elName= document.getElementById('name');
+
+elName.textContent=username;
+
+var elNote=document.getElementById('note');
+
+elNote.textContent=message;
+
